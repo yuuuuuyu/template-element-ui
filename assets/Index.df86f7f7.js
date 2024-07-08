61d8dec1946662dc7cc970f4b8ae909b9b8f0a2d
yuuuuuyu <44218347+yuuuuuyu@users.noreply.github.com>
2024-07-08 04:34:53 +0000

    Deploying to gh-pages from @ yuuuuuyu/template-element-ui@b079967bd81855afcefd7e47391fc78ec01398a8 🚀

---
+++ assets/Index.df86f7f7.js
@@ -0,0 +1,1 @@
+import{n as t}from"./index.346c828d.js";import"./ui.1d6f8af4.js";const s=t({data:()=>({}),methods:{},created:function(){}},(function(){return(0,this._self._c)("router-view",{staticClass:"scrollbar"})}),[],!1,null,"3860c9e8",null,null).exports;export{s as default};
